bba3e6eb89ef0e0322536c8cc30d44230956776d
Yousra Addali <yosraaddali@gmail.com>
2023-05-03 16:21:51 +0200

    Update investments.sql

--- easy/investments.sql
+++ easy/investments.sql
@@ -10,3 +10,29 @@ WHERE tiv_2015 IN (SELECT tiv_2015 FROM insurance i2
                    WHERE i1.pid != i2.pid)
 AND (lat, lon) NOT IN (SELECT lat, lon FROM insurance i3
                        WHERE i3.pid != i1.pid)
+
+
+# window function
+
+SELECT *,
+COUNT(*) OVER(PARTITION BY TIV_2015) AS CNT1,
+COUNT(*) OVER(PARTITION BY LAT, LON) AS CNT2
+FROM INSURANCE
+
+| pid | tiv_2015 | tiv_2016 | lat | lon | CNT1 | CNT2 |
+| --- | -------- | -------- | --- | --- | ---- | ---- |
+| 1   | 10       | 5        | 10  | 10  | 3    | 1    |
+| 3   | 10       | 30       | 20  | 20  | 3    | 2    |
+| 2   | 20       | 20       | 20  | 20  | 1    | 2    |
+| 4   | 10       | 40       | 40  | 40  | 3    | 1    |
+
+SELECT ROUND(SUM(TIV_2016),2) AS TIV_2016
+FROM
+(SELECT *,
+COUNT(*) OVER(PARTITION BY TIV_2015) AS CNT1,
+COUNT(*) OVER(PARTITION BY LAT, LON) AS CNT2
+FROM INSURANCE
+) AS TBL
+WHERE TBL.CNT1 >= 2 AND TBL.CNT2 = 1
+
+
